4463ea125e4a37ec5c3dbb1604910eadbbc93281
Amirhossein Douzandeh <amirzenoozi72@gmail.com>
2022-03-23 15:57:03 +0430

    :sparkles: Check Images Directory Using CLI

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 from keras.models import load_model
 
 import numpy as np
+import argparse
 import warnings
 import os
 
 import script.utils
 
-warnings.filterwarnings("ignore")
-model = load_model("model/model.h5")
-frames_path = 'data/frames/' 
+def parse_args():
+    desc = "Frame Extractor"
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('--model', type=str, default='model/model.h5', help='Where Is Model File?')
+    parser.add_argument('--dir', type=str, default='data/frames', help='What Is Images Directory?')
+    parser.add_argument('--json', type=str, default='data/frames', help='Where Should I Save The JSON File?')
 
-response  = {}
-frames = os.listdir(frames_path)
-for item in frames:
-        if os.path.isfile(frames_path+item):
-            image = script.utils.load_image(frames_path+item)
-            ans = model.predict(image)
-            maping = {0 : "Neutral", 1 : "Porn", 2 : "Sexy"}
-            new_ans = np.argmax(ans[0])
+    return parser.parse_args()
 
-            item_key_name = item.split('.')
-            response[item_key_name[0]] = {
-                "neutral": round((ans[0][0] * 100), 2),
-                "porn": round((ans[0][1] * 100), 2),
-                "sexy": round((ans[0][2] * 100), 2),
-            }
+def main():
+    args = parse_args()
+    if args is None:
+        exit()
 
-            # print(maping[new_ans], np.round(ans,2))
-            # print(f'{item}: With {ans[0][new_ans]} probability')
 
-script.utils.write_json_file(response, "result.json")
+    warnings.filterwarnings("ignore")
+    model = load_model(args.model)
+    frames_path = args.dir
+
+    response  = {}
+    frames = os.listdir(frames_path)
+    folder_name = os.path.basename(frames_path)
+    for item in frames:
+            if os.path.isfile(f'{frames_path}/{item}'):
+                image = script.utils.load_image(f'{frames_path}/{item}')
+                ans = model.predict(image)
+                maping = {0 : "Neutral", 1 : "Porn", 2 : "Sexy"}
+                new_ans = np.argmax(ans[0])
+
+                item_key_name = item.split('.')
+                response[item_key_name[0]] = {
+                    "neutral": round((ans[0][0] * 100), 2),
+                    "porn": round((ans[0][1] * 100), 2),
+                    "sexy": round((ans[0][2] * 100), 2),
+                }
+
+                # print(maping[new_ans], np.round(ans,2))
+                # print(f'{item}: With {ans[0][new_ans]} probability')
+
+    script.utils.write_json_file(response, f'{args.json}/{folder_name}_result.json')
+
+if __name__ == '__main__':
+    main()
