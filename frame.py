b0e9490711a21fc207087220bfc04ababa754714
Amirhossein Douzandeh <amirzenoozi72@gmail.com>
2022-03-23 15:56:43 +0430

    :sparkles: Extract Frame From File With CLI

--- frame.py
+++ frame.py
@@ -1,21 +1,47 @@
-# Opens the Video file
 import cv2
+import os
+import argparse
 
-cap= cv2.VideoCapture('data/video/eva-veil-1.ts')
-count = 0
-file_number = 0
+"""parsing and configuration"""
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
+def parse_args():
+    desc = "Frame Extractor"
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('--frame', type=int, default=1800, help='We Will Extract Frame Every X Frame')
+    parser.add_argument('--src', type=str, default='sample.mp4', help='What Is a Video Path?')
 
-    if ret:
-        cv2.imwrite('data/frames/frame{:d}.jpg'.format( file_number ), frame)
-        file_number += 1
-        count += 990 # i.e. at 30 fps, this advances one second
-        cap.set(1, count)
-    else:
-        cap.release()
-        break
+    return parser.parse_args()
 
-cap.release()
-cv2.destroyAllWindows()
+def main():
+    args = parse_args()
+    if args is None:
+        exit()
+    
+    # Opens the Video file
+    target_directory = 'data/frames'
+    file_name = os.path.basename(args.src).split(".")[0]
+    cap= cv2.VideoCapture(args.src)
+    count = 0
+    file_number = 0
+
+    if not os.path.exists(f'{target_directory}/{file_name}'):
+        os.makedirs(f'{target_directory}/{file_name}')
+        print('Frames Directory Created Successfully!')
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+
+        if ret:
+            cv2.imwrite(f'{target_directory}/{file_name}/frame{file_number}.jpg', frame)
+            file_number += 1
+            count += args.frame # i.e. at 30 fps, this advances one second
+            cap.set(1, count)
+        else:
+            cap.release()
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
